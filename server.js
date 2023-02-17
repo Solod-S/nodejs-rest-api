e8b5bfb804a65ff5fcd5e1fa8666e5066a73ba16
Sergey <104456968+Solod-S@users.noreply.github.com>
2023-02-17 08:35:49 +0200

    config + chalk

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const app = require("./app");
 const mongoose = require("mongoose");
+const chalk = require("chalk");
 
 const { DB_HOST, PORT = 3000 } = process.env;
+
+const errorMsg = chalk.bgKeyword("white").redBright;
+const successMsg = chalk.bgKeyword("green").white;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    console.log("Database connection successful");
+    console.log(successMsg("Database connection successful"));
   })
   .then(() =>
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running. Use our API on port: ${process.env.PORT}`);
+    app.listen(PORT, () => {
+      console.log(successMsg(`Server running. Use our API on port: ${PORT}`));
     })
   )
   .catch((error) => {
-    console.log(error);
+    console.log(errorMsg(error));
     process.exit(1);
   });
